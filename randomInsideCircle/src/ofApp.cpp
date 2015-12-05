c74fa6a7614c44e117fa4f7b21a86e5eb927677f
藤賀 雄太 <yuta.toga@gmail.com>
2015-12-06 00:41:33 +0900

    first commit

---
+++ randomInsideCircle/src/ofApp.cpp
@@ -0,0 +1,210 @@
+#include "ofApp.h"
+
+// ispired by http://marupeke296.com/TIPS_No18_Randoms.html
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetCircleResolution(128);
+    ofSetSphereResolution(4);
+    ofEnableDepthTest();
+    seedRandom = 1;
+    timerTime = 0;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    if ((int)ofGetElapsedTimef() % 3 == 0 && (ofGetElapsedTimef() - timerTime) > 1) {
+        seedRandom = (int)ofRandom(-100, 100);
+        timerTime = ofGetElapsedTimef();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    // draw as ofSeedRandom()
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/4.0*1-ofGetWidth()/4.0/2.0, ofGetHeight()/2.0-ofGetHeight()/2.0/2.0);
+    ofDrawBitmapStringHighlight("drawRandomCircle", -60, ofGetWidth()/4.0/2.0);
+    drawRandomCircle(ofVec2f(0, 0), ofGetWidth()/4.0/2.0*0.8, 100);
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/4.0*2-ofGetWidth()/4.0/2.0, ofGetHeight()/2.0-ofGetHeight()/2.0/2.0);
+    ofDrawBitmapStringHighlight("drawAverageRandomCircle", -90, ofGetWidth()/4.0/2.0);
+    drawAverageRandomCircle(ofVec2f(0, 0), ofGetWidth()/4.0/2.0*0.8, 100);
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/4.0*3-ofGetWidth()/4.0/2.0, ofGetHeight()/2.0-ofGetHeight()/2.0/2.0);
+    ofDrawBitmapStringHighlight("drawRandomSphere", -60, ofGetWidth()/4.0/2.0);
+    ofRotateY(ofGetFrameNum());
+    drawRandomSphere(ofVec3f(0, 0, 0), ofGetWidth()/4.0/2.0*0.8, 1000);
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/4.0*4-ofGetWidth()/4.0/2.0, ofGetHeight()/2.0-ofGetHeight()/2.0/2.0);
+    ofDrawBitmapStringHighlight("drawRandomSphereSurface", -80, ofGetWidth()/4.0/2.0);
+    ofRotateY(ofGetFrameNum());
+    drawRandomSphereSurface(ofVec3f(0, 0, 0), ofGetWidth()/4.0/2.0*0.8, 1000);
+    ofPopMatrix();
+    
+    // draw as ofSeedRandom(seedRandom)
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/4.0*1-ofGetWidth()/4.0/2.0, ofGetHeight()/2.0+ofGetHeight()/2.0/2.0);
+    ofDrawBitmapStringHighlight("drawRandomCircle", -60, ofGetWidth()/4.0/2.0);
+    ofSeedRandom(seedRandom);
+    drawRandomCircle(ofVec2f(0, 0), ofGetWidth()/4.0/2.0*0.8, 100);
+    ofSeedRandom();
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/4.0*2-ofGetWidth()/4.0/2.0, ofGetHeight()/2.0+ofGetHeight()/2.0/2.0);
+    ofDrawBitmapStringHighlight("drawAverageRandomCircle", -90, ofGetWidth()/4.0/2.0);
+    ofSeedRandom(seedRandom);
+    drawAverageRandomCircle(ofVec2f(0, 0), ofGetWidth()/4.0/2.0*0.8, 100);
+    ofSeedRandom();
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/4.0*3-ofGetWidth()/4.0/2.0, ofGetHeight()/2.0+ofGetHeight()/2.0/2.0);
+    ofDrawBitmapStringHighlight("drawRandomSphere", -60, ofGetWidth()/4.0/2.0);
+    ofRotateY(ofGetFrameNum());
+    ofSeedRandom(seedRandom);
+    drawRandomSphere(ofVec3f(0, 0, 0), ofGetWidth()/4.0/2.0*0.8, 1000);
+    ofSeedRandom();
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/4.0*4-ofGetWidth()/4.0/2.0, ofGetHeight()/2.0+ofGetHeight()/2.0/2.0);
+    ofDrawBitmapStringHighlight("drawRandomSphereSurface", -80, ofGetWidth()/4.0/2.0);
+    ofRotateY(ofGetFrameNum());
+    ofSeedRandom(seedRandom);
+    drawRandomSphereSurface(ofVec3f(0, 0, 0), ofGetWidth()/4.0/2.0*0.8, 1000);
+    ofSeedRandom();
+    ofPopMatrix();
+}
+
+void ofApp::drawAverageRandomCircle(ofVec2f center, float referenceCircleRadius, int randomPointNumber, bool enableSeedRandom){
+    // reference circle
+    ofNoFill();
+    ofDrawCircle(center.x, center.y, referenceCircleRadius);
+    
+    // random inside the reference circle
+    if (enableSeedRandom) {
+        ofSeedRandom(1);
+    }
+    ofFill();
+    for (int i = 0; i < randomPointNumber; i++) {
+        float theta = ofRandom(0, TWO_PI);
+        float randomCircleRadius = sqrt(ofRandom(0, 1))*referenceCircleRadius;
+        ofDrawCircle(center.x+randomCircleRadius*cos(theta), center.y+randomCircleRadius*sin(theta), 1);
+    }
+    ofSeedRandom();
+}
+
+void ofApp::drawRandomCircle(ofVec2f center, float referenceCircleRadius, int randomPointNumber, bool enableSeedRandom){
+    // reference circle
+    ofNoFill();
+    ofDrawCircle(center.x, center.y, referenceCircleRadius);
+    
+    // random inside the reference circle
+    if (enableSeedRandom){
+        ofSeedRandom(1);
+    }
+    ofFill();
+    for (int i = 0; i < randomPointNumber; i++) {
+        float theta = ofRandom(0, TWO_PI);
+        float randomCircleRadius = ofRandom(0, referenceCircleRadius);
+        ofDrawCircle(center.x+randomCircleRadius*cos(theta), center.y+randomCircleRadius*sin(theta), 1);
+    }
+    ofSeedRandom();
+}
+
+void ofApp::drawRandomSphereSurface(ofVec3f center, float referenceSphereRadius, int randomPointNumber, bool enableSeedRandom){
+    if (enableSeedRandom) {
+        ofSeedRandom(1);
+    }
+    for (int i = 0; i < randomPointNumber; i++) {
+        float theta = acos(1-2*ofRandom(0, 1));
+        float r = referenceSphereRadius*sin(theta);
+        float n = ofRandom(0, 1)*TWO_PI;
+        float x = referenceSphereRadius*cos(theta);
+        float y = r*sin(n);
+        float z = r*cos(n);
+        ofDrawSphere(x, y ,z, 1);
+    }
+    ofSeedRandom();
+}
+
+void ofApp::drawRandomSphere(ofVec3f center, float referenceSphereRadius, int randomPointNumber, bool enableSeedRandom){
+    if (enableSeedRandom) {
+        ofSeedRandom(1);
+    }
+    for (int i = 0; i < randomPointNumber; i++) {
+        float theta = acos(1-2*ofRandom(0, 1));
+        float r0 = sqrt(ofRandom(0, 1)) * referenceSphereRadius;
+        float r = r0*sin(theta);
+        float n = ofRandom(0, 1)*TWO_PI;
+        float x = r0*cos(theta);
+        float y = r*sin(n);
+        float z = r*cos(n);
+        ofDrawSphere(x, y ,z, 1);
+    }
+    ofSeedRandom();
+}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ randomInsideCircle/src/ofApp.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+    void drawAverageRandomCircle(ofVec2f center = ofVec2f(ofGetWidth()/2.0, ofGetHeight()/2.0), float referenceCircleRadius = 100, int randomPointNumber = 100, bool enableSeedRandom = false);
+    void drawRandomCircle(ofVec2f center = ofVec2f(ofGetWidth()/2.0, ofGetHeight()/2.0), float referenceCircleRadius = 100, int randomPointNumber = 100, bool enableSeedRandom = false);
+    void drawRandomSphereSurface(ofVec3f center, float referenceSphereRadius, int randomPointNumber, bool enableSeedRandom = false);
+    void drawRandomSphere(ofVec3f center, float referenceSphereRadius, int randomPointNumber, bool enableSeedRandom = false);
+    
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+    int seedRandom;
+    int timerTime;
+};
